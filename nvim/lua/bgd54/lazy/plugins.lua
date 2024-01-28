936a6f506bf63c0bb2ff964af8d350a274d641b9
bgd54 <balogh.dani54@gmail.com>
2024-01-28 22:06:31 +0100

    update which key

--- nvim/lua/bgd54/lazy/plugins.lua
+++ nvim/lua/bgd54/lazy/plugins.lua
@@ -1,41 +1,56 @@
 return {
-  -- TODO: LSP  harpoon2
-  -- CHECK: trouble, nvimd-dap, nvim-tree?, onedark
-  -- 'theprimeagen/harpoon',
+    -- TODO: harpoon2, test clangD lsp, ibl setup, comments, which-key
+    -- CHECK: trouble, nvimd-dap, nvim-tree?, onedark
+    -- 'theprimeagen/harpoon',
 
-  "folke/which-key.nvim",
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+    {
+        "folke/which-key.nvim",
+        opts = {},
+        config = function()
+            -- document existing key chains
+            require('which-key').register {
+                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+                ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+                ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+            }
+        end,
+    },
+    -- "gc" to comment visual regions/lines
+    { 'numToStr/Comment.nvim',               opts = {} },
 
-  'tpope/vim-surround',
-  {
-    'anuvyklack/pretty-fold.nvim',
-    config = function()
-      require('pretty-fold').setup()
-    end
-  },
+    'tpope/vim-surround',
+    {
+        'anuvyklack/pretty-fold.nvim',
+        config = function()
+            require('pretty-fold').setup()
+        end
+    },
 
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
-  {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'onedark',
-        },
-      }
-    end,
-  },
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = 'onedark',
+                },
+            }
+        end,
+    },
 
-  -- Colors
-  {
-      'rose-pine/neovim',
-      name = 'rose-pine',
-      config = function()
-          vim.cmd('colorscheme rose-pine')
-      end
-  },
-  'folke/tokyonight.nvim',
+    -- Colors
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
+    'folke/tokyonight.nvim',
 }
