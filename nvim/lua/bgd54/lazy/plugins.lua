572d4064f3ffbf6b953aa929a933cbac5c9f1d23
bgd54 <balogh.dani54@gmail.com>
2024-01-28 22:01:56 +0100

    add nvim to dotter and fix some configs

--- nvim/lua/bgd54/lazy/plugins.lua
+++ nvim/lua/bgd54/lazy/plugins.lua
@@ -19,7 +19,7 @@ return {
 
   {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     config = function()
       require('lualine').setup {
         options = {
--- nvim/lua/bgd54/remap.lua
+++ nvim/lua/bgd54/remap.lua
@@ -1,4 +1,5 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = '[J] Move lines up' })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = '[K] Move lines down' })
